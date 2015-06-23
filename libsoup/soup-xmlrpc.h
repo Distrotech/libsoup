c1aa9570999095bcc60fa8b7ab3a1eaf3fd730e8
Xavier Claessens <xavier.claessens@collabora.com>
2015-06-23 09:49:48 -0400

    Bump version to 2.51.0 and add SOUP_AVAILABLE_IN_2_52 on new APIs

--- libsoup/soup-xmlrpc.h
+++ libsoup/soup-xmlrpc.h
@@ -12,13 +12,16 @@
 G_BEGIN_DECLS
 
 /* XML-RPC client */
+SOUP_AVAILABLE_IN_2_52
 char       *soup_xmlrpc_build_request  (const gchar *method_name,
 					 GVariant    *params,
 					 GError     **error);
+SOUP_AVAILABLE_IN_2_52
 SoupMessage *soup_xmlrpc_message_new    (const char *uri,
 					 const char *method_name,
 					 GVariant    *params,
 					 GError     **error);
+SOUP_AVAILABLE_IN_2_52
 GVariant    *soup_xmlrpc_parse_response (const char *method_response,
 					 int         length,
 					 const char *signature,
@@ -26,35 +29,44 @@ GVariant    *soup_xmlrpc_parse_response (const char *method_response,
 
 /* XML-RPC server */
 typedef struct _SoupXMLRPCParams SoupXMLRPCParams;
+SOUP_AVAILABLE_IN_2_52
 void         soup_xmlrpc_params_free          (SoupXMLRPCParams  *self);
+SOUP_AVAILABLE_IN_2_52
 GVariant    *soup_xmlrpc_params_parse         (SoupXMLRPCParams  *self,
 					       const char       *signature,
 					       GError           **error);
+SOUP_AVAILABLE_IN_2_52
 char       *soup_xmlrpc_parse_request        (const gchar       *method_call,
 					       int               length,
 					       SoupXMLRPCParams **params,
 					       GError           **error);
+SOUP_AVAILABLE_IN_2_52
 char       *soup_xmlrpc_parse_request_full   (const gchar       *method_call,
 					       int               length,
 					       const char       *signature,
 					       GVariant         **parameters,
 					       GError           **error);
+SOUP_AVAILABLE_IN_2_52
 char       *soup_xmlrpc_build_response       (GVariant          *value,
 					       GError           **error);
 char       *soup_xmlrpc_build_fault          (int               fault_code,
 					       const char       *fault_format,
 					       ...) G_GNUC_PRINTF (2, 3);
+SOUP_AVAILABLE_IN_2_52
 gboolean     soup_xmlrpc_message_set_response (SoupMessage       *msg,
 					       GVariant          *value,
 					       GError           **error);
+SOUP_AVAILABLE_IN_2_52
 void         soup_xmlrpc_message_set_fault    (SoupMessage       *msg,
 					       int               fault_code,
 					       const char       *fault_format,
 					       ...) G_GNUC_PRINTF (3, 4);
 
 /* Utils */
+SOUP_AVAILABLE_IN_2_52
 GVariant *soup_xmlrpc_new_custom   (const char *type,
 				    const char *value);
+SOUP_AVAILABLE_IN_2_52
 GVariant *soup_xmlrpc_new_datetime (time_t       timestamp);
 
 G_END_DECLS
